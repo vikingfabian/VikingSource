8a66518f0590a710d13e14dc4f1ab711de91e846
Fabian Jakobsson <vikingfabian@hotmail.com>
2024-09-29 18:13:14 +0200

    BETA UPDATE - icons

--- VikingEngine/DSSWars/Data/Constants/DssVar.cs
+++ VikingEngine/DSSWars/Data/Constants/DssVar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace VikingEngine.DSSWars
         public static float SoldierGroup_Spacing;
         public static float Worker_StandardBoundRadius;
         public static float Men_StandardWalkingSpeed_PerSec;
+        public static Vector3 WorkerUnit_ResourcePosDiff;
         public static void UpdateConstants()
         {
             Projectile.Projectile_PeekHeight = DssConst.Men_StandardModelScale * 1f;
@@ -24,6 +26,7 @@ namespace VikingEngine.DSSWars
             SoldierGroup_Spacing = DssConst.SoldierGroup_RowWidth * DefaultGroupSpacing * 1.2f;
             Worker_StandardBoundRadius = StandardBoundRadius * 4f;
             Men_StandardWalkingSpeed_PerSec = DssConst.Men_StandardWalkingSpeed * TimeExt.SecondToMs;
+            WorkerUnit_ResourcePosDiff = new Vector3(0, DssConst.Men_StandardModelScale * 1.2f, DssConst.Men_StandardModelScale * 0.25f);
 
             BloodBlock.UpdateConstants();
         }
