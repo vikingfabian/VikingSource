16dba522c7d88fd51b66768c76384759f20980a2
Fabian Jakobsson <vikingfabian@hotmail.com>
2024-10-15 15:13:43 +0200

    subtile names

--- VikingEngine/DSSWars/Display/Translation/TodoTranslation.cs
+++ VikingEngine/DSSWars/Display/Translation/TodoTranslation.cs
@@ -9,6 +9,12 @@ namespace VikingEngine.DSSWars.Display.Translation
     class TodoTranslation
     {
         public string Resource_ReachedStockpile => "Reached stockpile goal buffer";
+
+        public string BuildingType_ResourceMine => "{0} mine";
+
+        public string Resource_TypeName_BogIron => "Bog iron";
+
+        public string Resource_TypeName_Coal => "Coal";
     }
 
 }
