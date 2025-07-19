2fd7bafcc14938fbed2e50d27861d9210d6aef21
Fabian Jakobsson <vikingfabian@hotmail.com>
2025-07-19 13:52:26 +0200

    draw lab start

---
+++ VikingEngine/DSSWars/GameState/ShaderLab/LabDraw.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VikingEngine.Engine;
+
+namespace VikingEngine.DSSWars.GameState.ShaderLab
+{
+    class LabDraw : Engine.Draw
+    {
+        public LabDraw():base() 
+        { 
+            ClrColor = ColorExt.VeryDarkGray;
+        }
+
+        protected override void drawEvent()
+        {
+            Ref.draw.AddToContainer = null;
+
+            spriteBatch.GraphicsDevice.Clear(ClrColor);
+            Draw2d((int)RenderLayer.Layer2);
+
+            Camera.RecalculateMatrices();
+            Camera.updateBillboard();
+            DrawGenerated(0, 0);
+
+            Draw3d(0, 0);
+
+            ParticleHandler.Draw(Camera);
+
+            Draw2d(0);
+        }
+    }
+}
---
+++ VikingEngine/DSSWars/GameState/ShaderLab/ShaderLabScene.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VikingEngine.DSSWars.GameState.CharacterCreator;
+using VikingEngine.DSSWars.Players.Profile;
+using VikingEngine.Engine;
+using VikingEngine.EngineSpace.HUD.RichBox.Artistic;
+using VikingEngine.HUD.RichBox.Artistic;
+using VikingEngine.HUD.RichBox;
+using VikingEngine.HUD.RichMenu;
+using Microsoft.Xna.Framework;
+
+namespace VikingEngine.DSSWars.GameState.ShaderLab
+{
+    class ShaderLabScene : Engine.GameState
+    {
+        RichMenu menu;
+        public ShaderLabScene() 
+            :base()
+        { 
+            openMenu();
+        }
+        protected override void createDrawManager()
+        {
+            draw = new LabDraw();
+        }
+        void openMenu()
+        {
+            if (menu == null)
+            {
+
+                var objectMenuArea = Screen.SafeArea;
+                objectMenuArea.Width = (int)(Engine.Screen.IconSize * 9f);
+
+                menu = new RichMenu(HudLib.RbSettings, objectMenuArea, new Vector2(8), RichMenu.DefaultRenderEdge, HudLib.GUILayer, XGuide.LocalHost);
+                var bgTex = menu.addBackground(HudLib.HudMenuBackground, HudLib.GUILayer + 2);
+
+                bgTex.SetColor(ColorExt.GrayScale(0.9f));
+                mainMenu();
+
+
+            }
+        }
+
+
+
+        void mainMenu()
+        {
+            RichBoxContent content = new RichBoxContent();
+            content.h1("Shader lab", HudLib.TitleColor_Head);
+
+
+            Refresh(content);
+        }
+
+        public void Refresh(RichBoxContent content)
+        {
+            //openMenu();
+            menu.Refresh(content);
+        }
+    }
+}
