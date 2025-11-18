69b6059ca52fa079e4052da84f68c446a5889378
Fabian Jakobsson <vikingfabian@hotmail.com>
2025-11-18 13:19:34 +0100

    city write fix

--- VikingEngine/DSSWars/GameObject/MapObj/City.cs
+++ VikingEngine/DSSWars/GameObject/MapObj/City.cs
@@ -38,6 +38,8 @@ namespace VikingEngine.DSSWars.GameObject
 {
     partial class City : AbsArmy
     {
+        const int MaxWorkerWriteCount = 64;
+
         public int areaSize = 0;
         public CityType cityType;
         public List<int> neighborCities = new List<int>();
@@ -490,7 +492,7 @@ namespace VikingEngine.DSSWars.GameObject
 
                 writeResources(w);
 
-                writeWorkerStatuses(w, false);
+                writeWorkerStatuses(w, false, -1);
 
                 w.Write((ushort)conscriptBuildings.Count);
                 foreach (var barracks in conscriptBuildings)
@@ -621,7 +623,7 @@ namespace VikingEngine.DSSWars.GameObject
 
             readResources(r, subversion);
             
-            readWorkerStatuses(r, false, subversion);
+            readWorkerStatuses(r, false, -1, subversion);
 
             refreshCitySize();
             conscriptBuildings.Clear();
@@ -740,28 +742,57 @@ namespace VikingEngine.DSSWars.GameObject
             Debug.ReadCheck(r);
         }
 
-        private void writeWorkerStatuses(BinaryWriter w, bool netPacket)
+        void writeStatusesStartEnd(int part, int workerStatusesCount, out bool meta, out int start, out int end)
         {
+            if (part < 0)
+            {
+                meta = true;
+                start = 0;
+                end = workerStatusesCount;
+            }
+            else
+            {
+                meta = part == 0;
+                start = part * MaxWorkerWriteCount;
+                end = Math.Min(workerStatusesCount, start + MaxWorkerWriteCount);
+            }
+        }
+
+        private void writeWorkerStatuses(BinaryWriter w, bool netPacket, int part)
+        {
+
             w.Write((ushort)workerStatuses.Count);
-            cityHallSubtilePos.write(w);
-            for (int i = 0; i < workerStatuses.Count; i++)
+            writeStatusesStartEnd(part, workerStatuses.Count, out bool meta, out int start, out int end);
+
+            if (meta)
+            {   
+                cityHallSubtilePos.write(w);
+            }
+
+            //int start = part * MaxWorkerWriteCount;
+            //int end = Math.Min(workerStatuses.Count, start + MaxWorkerWriteCount);
+            for (int i = start; i < end; i++)
             {
                 workerStatuses[i].writeGameState(this, w, netPacket);
             }
         }
 
-        private void readWorkerStatuses(BinaryReader r, bool netPacket, int subversion)
+        private void readWorkerStatuses(BinaryReader r, bool netPacket, int part, int subversion)
         {
             IntVector2 startPos = WP.ToSubTilePos_Centered(tilePos);
 
             int workerStatusesCount = r.ReadUInt16();
-
-            if (subversion >= 65)
+            writeStatusesStartEnd(part, workerStatusesCount, out bool meta, out int start, out int end);
+            
+            if (meta)
             {
-                cityHallSubtilePos.read(r);
+                if (subversion >= 65)
+                {
+                    cityHallSubtilePos.read(r);
+                }
             }
 
-            for (int i = 0; i < workerStatusesCount; i++)
+            for (int i = start; i < end; i++)
             {
                 WorkerStatus readWorker = new WorkerStatus()
                 {
@@ -994,15 +1025,16 @@ namespace VikingEngine.DSSWars.GameObject
             if (lastNetUpdate.secPassed(10))
             {
                 lastNetUpdate.setNow();
-                for (int part = 0; part < 2; ++part)
+
+                int count = MathExt.Div_Ceiling(workerStatuses.Count, MaxWorkerWriteCount) + 1;
+
+                for (int part = 0; part < count; ++part)
                 {
                     var w = Ref.netSession.BeginWritingPacket_Asynch(Network.PacketType.DssCityStatus, Network.PacketReliability.Reliable, out var packet);
                     {
                         w.Write((ushort)myIndex);
                         w.Write((byte)part);
-                        writeNet_update(w, part);
-
-                        
+                        writeNet_update(w, part);                        
                     }
                     packet.CheckPacketLength();
                     packet.EndWrite_Asynch();
@@ -1022,8 +1054,8 @@ namespace VikingEngine.DSSWars.GameObject
                     writeResources(w);
                     break;
 
-                case 1:
-                    writeWorkerStatuses(w, true);
+                default:
+                    writeWorkerStatuses(w, true, part -1);
                     break;
             }
             
@@ -1040,8 +1072,8 @@ namespace VikingEngine.DSSWars.GameObject
                     readResources(r, int.MaxValue);
                     break;
 
-                case 1:
-                    readWorkerStatuses(r, true, int.MaxValue);
+                default:
+                    readWorkerStatuses(r, true, part - 1, int.MaxValue);
                     break;
             }
             
