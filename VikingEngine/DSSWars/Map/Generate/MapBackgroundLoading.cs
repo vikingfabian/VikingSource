fad9fde9b3f2b3eecacb0ff14abee3d0fb82ad83
Fabian Jakobsson <vikingfabian@hotmail.com>
2025-11-28 17:10:08 +0100

    Incread min size 16 > 160 (and wont crash anymore)

--- VikingEngine/DSSWars/Map/Generate/MapBackgroundLoading.cs
+++ VikingEngine/DSSWars/Map/Generate/MapBackgroundLoading.cs
@@ -36,7 +36,7 @@ namespace VikingEngine.DSSWars.Map.Generate
         WorldDataStorage storage;
         protected LoadingState loadingState = 0;
         bool abort = false;
-        GenerateMap dataGenerate = null;
+        public GenerateMap dataGenerate = null;
         GenerateMap postGenerate;
         int failCount = 0;
         bool generateSuccess =false;
