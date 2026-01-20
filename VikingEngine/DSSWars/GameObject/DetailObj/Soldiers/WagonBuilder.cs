a126391364d84c12fd85da340845576ee56f9c6c
Fabian Jakobsson <vikingfabian@hotmail.com>
2026-01-20 15:08:47 +0100

    none

--- VikingEngine/DSSWars/GameObject/DetailObj/Soldiers/WagonBuilder.cs
+++ VikingEngine/DSSWars/GameObject/DetailObj/Soldiers/WagonBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,7 @@ namespace VikingEngine.DSSWars.GameObject.DetailObj.Soldiers
         public WagonBuilder() 
             :base()
         { 
-            unitBuildType = UnitBuildType.ConscriptWagon;
-            
+            unitBuildType = UnitBuildType.ConscriptWagon;            
         }
     }
 
@@ -36,6 +36,7 @@ namespace VikingEngine.DSSWars.GameObject.DetailObj.Soldiers
     class WagonRiderModel : AbsDetailUnitAdvancedModel
     {
         Graphics.VoxelModelInstance animalmodel_left, animalmodel_right;
+        Vector3 leftAnimalPosDiff, rightAnimalPosDiff;
         WalkingAnimation horseWalkingAnimation;
         WalkingAnimation wagonRollAnimation;
         public WagonRiderModel(AbsSoldierUnit soldier)
@@ -44,7 +45,25 @@ namespace VikingEngine.DSSWars.GameObject.DetailObj.Soldiers
             animalmodel_left = DssRef.models.ModelInstance_drawbatch(modelName, modelScale);
             animalmodel_right = DssRef.models.ModelInstance_drawbatch(modelName, modelScale);
 
+            leftAnimalPosDiff = new Vector3(-0.076f, 0, 0.07f) * modelScale;
+            rightAnimalPosDiff = leftAnimalPosDiff;
+            rightAnimalPosDiff.X = -rightAnimalPosDiff.X;
+
             model = DssRef.models.ModelInstance_drawbatch( LootFest.VoxelModelName.wagon_light, DssConst.Men_StandardModelScale * 1.1f);
+        }
+
+        public override void update(AbsSoldierUnit soldier)
+        {
+            base.update(soldier);
+            WP.Rotation1DToQuaterion(model, soldier.rotation.Radians);
+
+            animalmodel_left.Rotation = model.Rotation;
+            animalmodel_left.position = model.Rotation.TranslateAlongAxis(
+                leftAnimalPosDiff, model.position);
+
+            animalmodel_right.Rotation = model.Rotation;
+            animalmodel_right.position = model.Rotation.TranslateAlongAxis(
+                rightAnimalPosDiff, model.position);
         }
     }
 }
--- VikingEngine/DSSWars/Presentation/Translation.cs
+++ VikingEngine/DSSWars/Presentation/Translation.cs
@@ -40,6 +40,7 @@ namespace VikingEngine.DSSWars.Presentation
 #if DEBUG
                     throw new NotImplementedException();
 #endif
+
                 case LanguageType.English:
                     return SpriteName.LangButton_English;
 
