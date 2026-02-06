cc52a729a7c8e1c4941a33e73c17af8a08987791
Fabian Jakobsson <vikingfabian@hotmail.com>
2026-02-06 12:25:56 +0100

    remove gamestate on Exit

--- VikingEngine/EngineSpace/Engine/ClassFrame/ExitGameStateThreads.cs
+++ VikingEngine/EngineSpace/Engine/ClassFrame/ExitGameStateThreads.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VikingEngine.EngineSpace.Engine.ClassFrame
+namespace VikingEngine.Engine
 {
     class ExitGameStateThreads : AbsUpdateable
     {
