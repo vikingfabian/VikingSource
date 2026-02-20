483e2d0762c3b20dfda86e7500541cf77ebf984c
Fabian Jakobsson <vikingfabian@hotmail.com>
2026-02-20 13:13:01 +0100

    steam fork working

--- VikingEngine/Program.cs
+++ VikingEngine/Program.cs
@@ -2,13 +2,10 @@
 using VikingEngine;
 using VikingEngine.SteamWrapping;
 
-//var steam = new VikingEngine.SteamWrapping.SteamManager();
+
 new SteamManager();
-//if (SteamAPI.Init())//PlatformSettings.SteamAPI)
-//{
-    
-//    //steam.Initialize();
-//}
+
 using var game = new VikingEngine.MainGame();
 game.Run();
+
 SteamAPI.Shutdown();
