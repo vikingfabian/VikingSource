459b304aaef76e2fcf964e45ac101dd0f2246eb2
Fabian Jakobsson <vikingfabian@hotmail.com>
2026-06-08 11:37:35 +0200

    start

---
+++ VikingEngine/EngineSpace/HUD/RichBox/Book/RichBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VikingEngine.EngineSpace.HUD.RichBox.Book
+{
+    //abstract class AbsRichBookMember
+    //{ 
+
+    //}
+
+    //class Txt
+
+    class RichBook
+    {
+        string[] test = new string[] {
+            "<h1>title",
+            "<p>text",
+            "<*>bullet point",
+            "<h2>subtitle",
+            "<p>text <img=pinIcon> more text",
+        };
+
+
+        //    public RichBook(params AbsRichBookMember[] array)
+        //        :base(array.Length)
+        //    { 
+        //        AddRange(array);
+        //    }
+    }
+}
