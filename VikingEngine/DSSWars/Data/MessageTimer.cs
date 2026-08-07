97de18ff90155f9b1bc3263fdcf292ba04ce91f5
Fabian Jakobsson <vikingfabian@hotmail.com>
2026-08-07 23:18:36 +0200

    MessageTimer start

---
+++ VikingEngine/DSSWars/Data/MessageTimer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VikingEngine.Sound;
+
+namespace VikingEngine.DSSWars.Data
+{
+    struct MessageTimer
+    {
+        public SoundContainerBase sound;
+        public TimeStamp lastPlayed;
+
+        public MessageTimer(SoundContainerBase sound)
+        { 
+            this.sound = sound;
+            lastPlayed = TimeStamp.None;
+        }
+    }
+}
