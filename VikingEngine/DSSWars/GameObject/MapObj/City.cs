1dcec7abe28b49ce9b5d4b0ac0c5c5fbc21ff578
Fabian Jakobsson <vikingfabian@hotmail.com>
2026-08-08 15:25:11 +0200

    * Fixed crashing font on renamed cities/armies

--- VikingEngine/DSSWars/GameObject/MapObj/City.cs
+++ VikingEngine/DSSWars/GameObject/MapObj/City.cs
@@ -2298,7 +2298,7 @@ namespace VikingEngine.DSSWars.GameObject
                 return TextLib.Error;
             }
             mayEdit = faction != null && faction.player != null && faction.player.IsLocalPlayer();
-            return name.name;
+            return name.GetName();
         }
 
         //public override void NameEditEvent(string result, object tag)
