daacd4d2ab6fef50977fd59328d93cbe31cb284d
Uygar Yilmaz <uygar.raf@gmail.com>
2026-08-30 20:16:50 -0400

    Optimizing path finding.
    Adding unit tests to ensure no regression, adding benchmarks to prove improvement.
    
    Fixing dependency version conflicts.
    
    Cleanup.
    
    Further cleanuup.
    
    Switching to <InternalsVisibleTo /> in csproj.
    
    Cleaning up local *.user files and adding them to gitignore.

---
+++ VikingEngine.Benchmarks/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+using VikingEngine.Benchmarks.Pathfinding;
+
+namespace VikingEngine.Benchmarks;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<PathFindingBenchmarks>();
+    }
+}
